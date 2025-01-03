03aeff70310b2547c4fbcaa69c1c9e5366dffe37
Nelson <nelsonfeng.18@gmail.com>
2025-01-03 11:40:28 -0500

    Import constants and images (tutorial hell)

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg my-10">Welcome to table42</Text>
+      <Text className="font-bold text-3xl font-rubik my-10">Welcome to table42</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
