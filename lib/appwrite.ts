85ff43b2b0b70cbfae28b32fe67055078aa37fc9
Nelson <nelsonfeng.18@gmail.com>
2025-01-04 15:15:11 -0500

    Integrate AUTH

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,6 +1,6 @@
-import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
-import * as Linking from 'expo-linking';
-import { openAuthSessionAsync } from "expo-web-browser";
+import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite"
+import * as Linking from 'expo-linking'
+import { openAuthSessionAsync } from "expo-web-browser"
 
 
 export const config = {
@@ -60,7 +60,7 @@ export async function logout() {
   }
 }
 
-export async function getUser() {
+export async function getCurrentUser() {
   try {
     const response = await account.get();
 
